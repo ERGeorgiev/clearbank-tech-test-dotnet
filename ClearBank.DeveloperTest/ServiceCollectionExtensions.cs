1e0f3822981f2c2e6e88343976f3b40768ecc72a
Eduard <e.r.georgiev@gmail.com>
2026-05-08 21:22:20 +0100

    AddPaymentServices implementation

--- ClearBank.DeveloperTest/ServiceCollectionExtensions.cs
+++ ClearBank.DeveloperTest/ServiceCollectionExtensions.cs
@@ -1,3 +1,7 @@
+using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using ClearBank.DeveloperTest.Validators;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ClearBank.DeveloperTest;
@@ -6,6 +10,17 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddPaymentServices(this IServiceCollection services)
     {
+        services.AddScoped<IPaymentService, PaymentService>();
+
+        services.AddScoped<IAccountDataStoreFactory, AccountDataStoreFactory>();
+        services.AddKeyedScoped<IAccountDataStore, AccountDataStore>(DataStoreType.Default);
+        services.AddKeyedScoped<IAccountDataStore, BackupAccountDataStore>(DataStoreType.Backup);
+
+        services.AddScoped<IPaymentValidatorFactory, PaymentValidatorFactory>();
+        services.AddKeyedScoped<IPaymentValidator, BacsPaymentValidator>(PaymentScheme.Bacs);
+        services.AddKeyedScoped<IPaymentValidator, FasterPaymentsPaymentValidator>(PaymentScheme.FasterPayments);
+        services.AddKeyedScoped<IPaymentValidator, ChapsPaymentValidator>(PaymentScheme.Chaps);
+
         return services;
     }
 }
